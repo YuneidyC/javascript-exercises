63514afd9ddd05944c154b7378beb81fea584cb7
YuneidyC <yuneidycastillo25@gmail.com>
2025-02-25 17:11:05 +0000

    Add 'Exercise 02 - repeatString' solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0)
+    {
+        return "ERROR";
+    } else if (num === 0)
+    {
+        return "";
+    }
 
+    let string = "";
+    for (let i = 0; i < num; i++)
+    {
+        string += str;
+    }
+
+    return string;
 };
 
 // Do not edit below this line
