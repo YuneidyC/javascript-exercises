faf7ffa66732664938a2c7b41296a1ffa4b49f34
YuneidyC <yuneidycastillo25@gmail.com>
2025-03-07 17:20:20 +0000

    Enable tests for "09  - Palindromes"
    Should have been done in a previous commit

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function (str) {
+    str = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let j = str.length - 1;
+
     for (let i = 0; i < str.length / 2; i++)
     {
         if (str[i] !== str[j])
@@ -10,6 +12,8 @@ const palindromes = function (str) {
     }
     return true;
 };
+
+palindromes('Rac ecar!');
 
 // Do not edit below this line
 module.exports = palindromes;
