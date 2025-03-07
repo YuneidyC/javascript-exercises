11e99224d65485b9b4dc635831d5f5b664c5c91d
YuneidyC <yuneidycastillo25@gmail.com>
2025-03-07 18:17:24 +0000

    Add 'Exercise 10 - Fibonacci' solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if (num < 0)
+    {
+        return "OOPS";
+    }
 
+    let fib = [0, 1];
+    num = parseInt(num);
+
+    if (num === 0 || num === 1)
+    {
+        return num;
+    }
+
+    for (let i = 2; i <= num; i++)
+    {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+
+    fib = fib[fib.length - 1];
+    return fib;
 };
 
 // Do not edit below this line
