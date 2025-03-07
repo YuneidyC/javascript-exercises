5a3b933aec7bc8bc59468bcceee890cf0fcab9f0
YuneidyC <yuneidycastillo25@gmail.com>
2025-03-07 14:12:11 +0000

    Enable tests for "05 - Sum all"
    Should have been done in a previous commit

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,12 +1,29 @@
-const sumAll = function(first, last) {
+const sumAll = function (first, second) {
+
+    if (!checkIsAInteger(first) || !checkIsAInteger(second))
+    {
+        return "ERROR";
+    }
+
+    if (first > second)
+    {
+        let changeValue = first;
+        first = second;
+        second = changeValue;
+    }
+
     let total = 0;
-    for (let i = first; i <= last; i++)
+    for (let i = first; i <= second; i++)
     {
         total += i;
     }
 
     return total;
 };
+
+function checkIsAInteger(num) {
+    return typeof num === "number" && num % 1 === 0 && num > 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
