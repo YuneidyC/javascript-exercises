4199feedd4b491825b002b4addc34273a22f5d29
YuneidyC <yuneidycastillo25@gmail.com>
2025-03-07 13:15:43 +0000

    Add 'Exercise 04 - Remove From Array' solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...num) {
+    const allNums = new Set(num);
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (!allNums.has(arr[i]))
+        {
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
 };
+
+removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
